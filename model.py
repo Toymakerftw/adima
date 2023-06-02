3f4a81ac920103484344b1fa1a3c677fe752c292
Toymakerftw <anandhraman@sngce.ac.in>
2023-06-02 14:15:24 +0530

    triage: hmm

--- model.py
+++ model.py
@@ -1,35 +1,9 @@
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO
-import psutil
-import subprocess
-import time
-import sqlite3
 import xgboost as xgb
 import pandas as pd
 from scapy.all import *
-from scheduler import start_scheduler
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your-secret-key'
-socketio = SocketIO(app)
-
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('Client disconnected')
-
-def get_network_devices():
-    """
-    Get the number of devices on the network using ARP.
-    """
-    arp_output = subprocess.check_output(['arp', '-a']).decode('utf-8')
-    arp_lines = arp_output.split('\n')
-    device_lines = [line for line in arp_lines if ' at ' in line]
-    num_devices = len(device_lines)
-    return num_devices
 
 def extract_features(packet):
     features = {}
@@ -122,40 +96,8 @@ def analyze_pcap(pcap_file, model):
     
     return results, malicious_nodes, device_stats
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/firewall')
-def firewall():
-    # Connect to the SQLite database
-    conn = sqlite3.connect('triage.db')
-    cursor = conn.cursor()
-
-    # Fetch IP and MAC addresses from the mal_mac table
-    cursor.execute('SELECT ip, mac FROM mal_mac')
-    devices = cursor.fetchall()
-
-    # Close the database connection
-    conn.close()
-
-    # Render the template with the devices data
-    return render_template('firewall.html', devices=devices)
-
-@app.route('/block_device', methods=['POST'])
-def block_device():
-    mac = request.form.get('mac')
-
-    # Block the MAC address using firewalld
-    subprocess.run(['firewall-cmd', '--permanent', '--add-rich-rule',
-                    'rule family="ipv4" source address="{0}" drop'.format(mac)])
-    subprocess.run(['firewall-cmd', '--reload'])
-
-    # Return a JSON response
-    return {'message': 'Device blocked successfully'}
-
-@app.route('/pcap', methods=['GET', 'POST'])
-def pcap():
     if request.method == 'POST':
         # POST request: Analysis triggered by the button press
         
@@ -179,82 +121,6 @@ def pcap():
         
         return render_template('model.html', malicious_results=results, device_stats=device_statistics)
 
-@socketio.on('request_stats')
-def send_stats():
-    while True:
-        # Get the network devices count
-        num_devices = get_network_devices()
-
-        # Get the system uptime
-        uptime = get_system_uptime()
-
-        # Get the CPU usage
-        cpu_usage = get_cpu_usage()
-
-        # Get the memory usage
-        memory_usage = get_memory_usage()
-
-        # Get the storage usage
-        storage_usage = get_storage_usage()
-
-        # Emit the statistics to the client
-        socketio.emit('update_stats', {
-            'uptime': uptime,
-            'cpu_usage': cpu_usage,
-            'memory_usage': memory_usage,
-            'storage_usage': storage_usage,
-            'num_devices': num_devices
-        })
-
-        time.sleep(1)
-
-def get_system_uptime():
-    """
-    Get the system uptime in a human-readable format.
-    """
-    uptime = time.time() - psutil.boot_time()
-    minutes, seconds = divmod(uptime, 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-    return f"{int(days)} days, {int(hours)} hours, {int(minutes)} minutes, {int(seconds)} seconds"
-
-def get_cpu_usage():
-    """
-    Get the current CPU usage as a percentage.
-    """
-    return psutil.cpu_percent(interval=1)
-
-def get_memory_usage():
-    """
-    Get the current memory usage in a human-readable format.
-    """
-    memory = psutil.virtual_memory()
-    return {
-        'total': memory.total,
-        'available': memory.available,
-        'used': memory.used,
-        'percent': memory.percent
-    }
-
-def get_storage_usage():
-    """
-    Get the current storage usage in a human-readable format.
-    """
-    partitions = psutil.disk_partitions()
-    usage = {}
-    for partition in partitions:
-        try:
-            partition_usage = psutil.disk_usage(partition.mountpoint)
-            usage[partition.device] = {
-                'total': partition_usage.total,
-                'used': partition_usage.used,
-                'free': partition_usage.free,
-                'percent': partition_usage.percent
-            }
-        except Exception as e:
-            print(f"Error retrieving usage information for {partition.device}: {e}")
-    return usage
 
 if __name__ == '__main__':
-    start_scheduler()
-    socketio.run(app)
+    app.run(debug=True)
