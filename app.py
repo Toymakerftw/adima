05edaaadfbfcd2ba4bf46979a5ac05ecb181a774
Toymakerftw <anandhraman@sngce.ac.in>
2023-06-11 11:05:54 +0530

    triage: Add Sqlite3 DB

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Import the necessary libraries
 from flask import Flask, render_template, request
 import xgboost as xgb
 import pandas as pd
@@ -6,12 +7,52 @@ import socket
 from scapy.layers.inet import IP
 from scapy.all import *
 import os
+import sqlite3
+from sqlite3 import Error
 
 app = Flask(__name__)
 
 # Load the saved model
 model = xgb.Booster()
 model.load_model("unsw_nb15_xgb_model2.model")
+
+
+# Create a connection pool
+def create_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect("triage.db")
+        return conn
+    except Error as e:
+        print(e)
+
+
+# Connect to the SQLite database and create the "mal_ip" table
+def create_table(conn):
+    try:
+        cursor = conn.cursor()
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS mal_ip (
+                Source TEXT,
+                Destination TEXT
+            )
+        """
+        )
+        conn.commit()
+        return cursor
+    except Error as e:
+        print(e)
+
+
+# Insert malicious IPs into the "mal_ip" table
+def insert_malicious_ips(cursor, src_ip, dst_ip):
+    try:
+        cursor.execute(
+            "INSERT INTO mal_ip (Source, Destination) VALUES (?, ?)", (src_ip, dst_ip)
+        )
+    except Error as e:
+        print(e)
 
 
 @app.route("/")
@@ -25,9 +66,8 @@ def upload():
     if "pcap_file" not in request.files:
         return "No file uploaded"
 
-    file = request.files["pcap_file"]
-
     # Save the uploaded file
+    file = request.files["pcap_file"]
     file.save("uploaded.pcap")
 
     # Read the pcap file and extract packet data
@@ -75,12 +115,27 @@ def upload():
     dtest = xgb.DMatrix(df)
     predictions = model.predict(dtest)
 
+    # Create a connection from the connection pool
+    conn = create_connection()
+    cursor = None
+    if conn is not None:
+        # Create the "mal_ip" table if it doesn't exist
+        cursor = create_table(conn)
+
     # Filter out the malicious packets
     malicious_ips = []
     for i in range(len(predictions)):
         if predictions[i] == 1:
             if IP in packets[i]:
                 malicious_ips.append((packets[i][IP].src, packets[i][IP].dst))
+                # Insert the malicious IPs into the "mal_ip" table
+                if cursor is not None:
+                    insert_malicious_ips(cursor, packets[i][IP].src, packets[i][IP].dst)
+                    conn.commit()
+
+    # Close the connection
+    if conn is not None:
+        conn.close()
 
     # Render the result.html template with the malicious IPs
     return render_template("result.html", malicious_ips=malicious_ips)
